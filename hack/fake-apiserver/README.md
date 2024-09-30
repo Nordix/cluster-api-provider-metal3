9226ca0a8a69615ce6d94ad73822075710884b1b
Huy Mai <huy.mai@est.tech>
2024-09-30 06:03:08 +0000

    Add fkas-metal3-watcher
    Signed-off-by: Huy Mai <huy.mai@est.tech>

--- hack/fake-apiserver/README.md
+++ hack/fake-apiserver/README.md
@@ -1,47 +1,42 @@
-# Fake API server
+# Metal3 Fake Kubernetes API server system (Metal3-FKAS)
 
-Fake API server is a tool running inside a kubernetes cluster,
-and generates "fake" k8s api server endpoints on demand.
+## FKAS
 
-## Purpose
+Metal3-FKAS tool for testing CAPI-related projects.
+When being asked, it generates new fake kubernetes api endpoint, that responds
+to all typical requests that CAPI sends to a newly provisioned cluster.
 
-When CAPI+CAPM3 provisions a baremetal node, after BMO and Ironic finished
-provisioning the node, a node image with kubernetes components (kubelet,
-containerd, etcd, etc.) is supposed to be installed inside the new node,
-and as it boots up afterwards, it will start itself up as a kubernetes node.
-At the end of the process, CAPI will send queries towards the newly launched
-cluster's API server to verify that the cluster is fully up and running.
-The address of this API server is defined in `CLUSTER_APIENDPOINT_HOST`
-and `CLUSTER_APIENDPOINT_PORT` variables, which users need to provide to the
-cluster template.
+Despite being developed for Metal3 ecosystem, FKAS is provider-free. It can be adopted
+and used by any CAPI provider with intention to test the provider provisioning ability,
+without using real nodes.
 
-In a test setup with fake nodes, i.e. nodes that only "exist" in either ironic database,
-(like in case of FakeIPA system), or nodes faked by BMO in its simulation mode,
-the "nodes" are not able to boot up any kubernetes api server,
-hence the needs of having mock API servers on-demands.
+### Purpose
 
-## How it works
+After a CAPI Infrastructure provisions a new cluster, CAPI will send queries
+towards the newly launched cluster's API server to verify that the cluster is
+fully up and running.
 
-After a BMH associating to a fake node is provisioned to `available` state,
-the user can send a request towards the Fake API server endpoint `/register`,
-which will spawn a new API server, with an unique IP address.
+In a simulated provisioning process, there are no real nodes, hence we cannot
+have an actual API server running inside the node. Booting up a real kubelet
+and etcd servers elsewhere is possible, but these processes are likely to consume
+a lot of resources.
 
-The generated API server endpoint responds to any request normally responded
-by the apiserver of target clusters created in a normal CAPI workflow.
+FKAS is useful in this situation. When a request is sent towards `/register` endpoint,
+it will spawn a new simulated kubernetes API server with *unique* a host and
+port pair.
+User can, then, inject the address into cluster template consumed by
+CAPI with any infra provider.
 
-User can, then, use this IP address to feed the cluster template, and start
-"provisioning" the node to a kubernetes cluster.
+### How to use
 
-## How to use
-
-You can build the `fake-api-server` image that is suitable for
+You can build the `metal3-fkas` image that is suitable for
 your local environment with
 
 ```shell
-make build-fake-api-server
+make build-fkas
 ```
 
-The result is an image with label `quay.io/metal3-io/fake-apiserver:<your-arch-name>`
+The result is an image with label `quay.io/metal3-io/metal3-fkas:<your-arch-name>`
 
 Alternatively, you can also build a custom image with
 
@@ -61,29 +56,29 @@ minikube image load /tmp/api-server.tar
 Now you can deploy this container to the cluster, for e.g. with a deployment
 
 ```yaml
-# api-server-deployment.yaml
+# fkas-deployment.yaml
 ---
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-  name: metal3-fake-api-server
+  name: fkas
   namespace: default
 spec:
-  replicas: 2
+  replicas: 1
   selector:
     matchLabels:
-      app: capim
   strategy:
+      app: metal3-fkas
     type: Recreate
   template:
     metadata:
       labels:
-        app: capim
+        app: metal3-fkas
     spec:
       containers:
-        - image: quay.io/metal3-io/api-server:amd64
+        - image: quay.io/metal3-io/metal3-fkas:amd64
           imagePullPolicy: IfNotPresent
-          name: capim
+          name: fkas
           env:
           - name: POD_IP
             valueFrom:
@@ -93,16 +88,16 @@ spec:
 ```
 
 ```shell
-kubectl apply -f api-server-deployment.yaml
+kubectl apply -f fkas-deployment.yaml
 ```
 
-After building the container image and deploy it to a kubernetes cluster,
+After building the container image and deploy it to the bootstrap kubernetes cluster,
 you need to create a tunnel to send request to it and get response, by using
 a LoadBalancer, or a simple port-forward
 
 ```shell
-api_server_name=$(kubectl get pods -l app=capim -o jsonpath="{.items[0].metadata.name}")
-kubectl port-forward pod/${api_server_name} ${api_server_port}:3333 2>/dev/null&
+fkas_pod_name=$(kubectl get pods -l app=metal3-fkas -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward pod/${fkas_pod_name} 3333:3333 2>/dev/null&
 ```
 
 Now, you can generate a fake API server endpoint by sending
@@ -120,23 +115,37 @@ caKeyEncoded=$(cat /tmp/ca.key | base64 -w 0)
 caCertEncoded=$(cat /tmp/ca.crt | base64 -w 0)
 etcdKeyEncoded=$(cat /tmp/etcd.key | base64 -w 0)
 etcdCertEncoded=$(cat /tmp/etcd.crt | base64 -w 0)
-namespace="metal3"
-cluster_name="test_cluster"
+namespace=<cluster-namespace>
+cluster_name=<cluster-name>
 
-cluster_endpoint=$(curl localhost:${api_server_port}/register?resource=$namespace/$cluster_name&caKey=$caKeyEncoded&caCert=$caCertEncoded&etcdKey=$etcdKeyEncoded&etcdCert=$etcdCertEncoded")
+cluster_endpoint=$(curl -X POST "localhost:3333/register" \
+-H "Content-Type: application/json" -d '{
+  "resource": "'$namespace/$cluster_name'",
+  "caKey": "'$caKeyEncoded'",
+  "caCert": "'$caCertEncoded'",
+  "etcdKey": "'$etcdKeyEncoded'",
+  "etcdCert": "'$etcdCertEncoded'"
+}')
 ```
 
 The fake API server will return a response with the ip and port of the newly
-generated api server. These information can be fed to a CAPI infrastructure provider
-(for e.g. CAPM3) to create a cluster. Notice that you need to manually create
-the `ca-secret` and `etcd-secret` of the new cluster, so that it matches the certs
-used by the api server.
+generated api server. For example:
+
+```json
+{
+  Host: "10.244.0.83",
+  Port: 20000
+}
+```
+
+We also need to manually create the `ca-secret` and `etcd-secret` of the new cluster,
+so that they match the certs used by the api server.
 
 ```shell
 host=$(echo ${cluster_endpoints} | jq -r ".Host")
 port=$(echo ${cluster_endpoints} | jq -r ".Port")
 
-  cat <<EOF > "/tmp/${cluster}-ca-secrets.yaml"
+cat <<EOF > "/tmp/${cluster}-ca-secrets.yaml"
 apiVersion: v1
 kind: Secret
 metadata:
@@ -150,9 +159,9 @@ data:
   tls.key: ${caKeyEncoded}
 EOF
 
-  kubectl -n ${namespace} apply -f /tmp/${cluster}-ca-secrets.yaml
+kubectl -n ${namespace} apply -f /tmp/${cluster}-ca-secrets.yaml
 
-  cat <<EOF > "/tmp/${cluster}-etcd-secrets.yaml"
+cat <<EOF > "/tmp/${cluster}-etcd-secrets.yaml"
 apiVersion: v1
 kind: Secret
 metadata:
@@ -166,16 +175,22 @@ data:
   tls.key: ${etcdKeyEncoded}
 EOF
 
-  kubectl -n ${namespace} apply -f /tmp/${cluster}-etcd-secrets.yaml
+kubectl -n ${namespace} apply -f /tmp/${cluster}-etcd-secrets.yaml
+```
 
-  # Generate metal3 cluster
-  export CLUSTER_APIENDPOINT_HOST="${host}"
-  export CLUSTER_APIENDPOINT_PORT="${port}"
-  echo "Generating cluster ${cluster} with clusterctl"
-  clusterctl generate cluster "${cluster}" \
-    --from "${CLUSTER_TEMPLATE}" \
-    --target-namespace "${namespace}" > /tmp/${cluster}-cluster.yaml
-  kubectl apply -f /tmp/${cluster}-cluster.yaml
+A new cluster can be provisioned by feeding a CAPI infrastructure provider
+(for e.g. CAPM3) with the host and port we got from FKAS.
+
+```shell
+# Injecting the new api address into the cluster
+export CLUSTER_APIENDPOINT_HOST="${host}"
+export CLUSTER_APIENDPOINT_PORT="${port}"
+
+#
+clusterctl generate cluster "${cluster}" \
+  --from "${CLUSTER_TEMPLATE}" \
+  --target-namespace "${namespace}" > /tmp/${cluster}-cluster.yaml
+kubectl apply -f /tmp/${cluster}-cluster.yaml
 ```
 
 After the cluster is created, CAPI will expect that information like node name
@@ -184,14 +199,64 @@ live inside the node, we will need to feed the info to it, by sending a
 GET request to `/updateNode` endpoint:
 
 ```shell
-curl "localhost:${api_server_port}/updateNode?resource=${namespace}/${cluster}&nodeName=${machine}&providerID=${providerID}"
+curl -X POST "localhost:3333/updateNode" -H "Content-Type: application/json" -d '{
+  "resource": "${namespace}/${cluster}",
+  "nodeName": "<machine-object-name>",
+  "providerID": "<provider-id>",
+  "uuid": "<node-uuid>",
+  "nodeType": "<node-type>"
+}'
 ```
 
-## Acknowledgements
+Here `nodeType` should be either `control-plane` or `worker` (In fact,
+anything not equals to `control-plane` will be treated as a worker)
+
+### Acknowledgements
 
 This was developed thanks to the implementation of
 [Cluster API Provider In Memory (CAPIM)](https://github.com/kubernetes-sigs/cluster-api/tree/main/test/infrastructure/inmemory).
 
-**NOTE:**:
+## Metal3 FKAS System
+
+### FKAS in Metal3
+
+In metal3 ecosystem, currently we have two ways of simulating a workflow without
+using any baremetal or virtual machines:
+
+- [FakeIPA container](https://github.com/metal3-io/utility-images/tree/main/fake-ipa)
+- BMO simulation mode
+
+In both of these cases, the "nodes" are not able to boot up any kubernetes api server,
+hence the needs of having mock API servers on-demands.
+
+Similar to the general case, after having BMHs provisioned to `available` state,
+the user can send a request towards the Fake API server endpoint `/register`,
+which will spawn a new API server, with an unique `Host` and `Port` pair.
+
+User can, then, use this IP address to feed the cluster template, by exporting
+`CLUSTER_APIENDPOINT_HOST` and `CLUSTER_APIENDPOINT_PORT` variables.
+
+There is no need of manually check and send node info to `/updateNode`, as we have
+another tool to automate that part.
+
+### Metal3-FKAS-Reconciler
+
+This tool runs as a side-car container alongside FKAS, and works specifically
+for Metal3. It eliminates the needs of user to manually fetch the nodes information
+and send to `/updateNode` (as described earlier), by constantly watch the changes
+in BMH objects, notice if a BMH is being provisioned to a kubernetes node, and
+send request to `updateNode` with appropriate information.
+
+### Deployment
+
+The `metal3-fkas-system` (including `fkas` and `metal3-fkas-reconciler`)
+can be deployed with the `k8s/metal3-fkas-system-deployment.yaml` file.
+
+```shell
+kubectl apply -f k8s/metal3-fkas-system-deployment.yaml
+```
+
+## Disclaimer
+
 This is intended for development environments only.
 Do **NOT** use it in production.
--- hack/fake-apiserver/cmd/metal3-fkas/main.go
+++ hack/fake-apiserver/cmd/metal3-fkas/main.go
@@ -5,7 +5,7 @@ import (
 	"crypto/rsa"
 	"encoding/base64"
 	"encoding/json"
-	"io"
+	"fmt"
 	"net/http"
 	"os"
 	"time"
@@ -13,16 +13,18 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/klog/v2"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	infrav1 "sigs.k8s.io/cluster-api/test/infrastructure/inmemory/api/v1alpha1"
 	cmanager "sigs.k8s.io/cluster-api/test/infrastructure/inmemory/pkg/runtime/manager"
 	"sigs.k8s.io/cluster-api/test/infrastructure/inmemory/pkg/server"
 	"sigs.k8s.io/cluster-api/util/certs"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
 var (
@@ -55,48 +57,70 @@ type ResourceData struct {
 // register receives a resourceName, ca and etcd secrets (key+cert) from a request
 // and generates a fake k8s API server corresponding with the provided name and secrets.
 func register(w http.ResponseWriter, r *http.Request) {
-	resourceName := r.URL.Query().Get("resource")
+	var requestData struct {
+		ResourceName string `json:"resource"`
+		CaKey        string `json:"caKey"`
+		CaCert       string `json:"caCert"`
+		EtcdKey      string `json:"etcdKey"`
+		EtcdCert     string `json:"etcdCert"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		setupLog.Error(err, "Invalid JSON in request body")
+		return
+	}
+
+	setupLog.Info("Registering new resource", "resource", requestData.ResourceName)
+	resourceName := requestData.ResourceName
 	resp := &ResourceData{}
 	resp.ResourceName = resourceName
+	setupLog.Info("Adding resource group", "resourceName", resourceName)
 	cloudMgr.AddResourceGroup(resourceName)
 	// NOTE: We are using resourceName as listener name for convenience
 	listener, err := apiServerMux.InitWorkloadClusterListener(resourceName)
 	if err != nil {
-		http.Error(w, "", http.StatusInternalServerError)
+		setupLog.Error(err, "Failed to initialize listener", "resourceName", resourceName)
+		http.Error(w, "Failed to initialize listener", http.StatusInternalServerError)
 		setupLog.Error(err, "failed to Initialize listener")
 		return
 	}
 	// NOTE: The two params are name of the listener and name of the resource group
 	// we are setting both of them to resourceName for convenience, but it is not required.
 	if err := apiServerMux.RegisterResourceGroup(resourceName, resourceName); err != nil {
-		http.Error(w, "", http.StatusInternalServerError)
+		setupLog.Error(err, "Failed to register resource group to listener", "resourceName", resourceName)
+		http.Error(w, "Failed to register resource group to listener", http.StatusInternalServerError)
 		setupLog.Error(err, "failed to Register resource group to listener")
 		return
 	}
-	caKeyEncoded := r.URL.Query().Get("caKey")
+	caKeyEncoded := requestData.CaKey
 	caKeyRaw, err := base64.StdEncoding.DecodeString(caKeyEncoded)
 	if err != nil {
-		http.Error(w, "", http.StatusInternalServerError)
+		setupLog.Error(err, "Failed to generate caKey", "resourceName", resourceName)
+		http.Error(w, "Failed to generate caKey", http.StatusInternalServerError)
 		setupLog.Error(err, "failed to generate caKey")
 		return
 	}
-	caCertEncoded := r.URL.Query().Get("caCert")
+	caCertEncoded := requestData.CaCert
 	caCertRaw, err := base64.StdEncoding.DecodeString(caCertEncoded)
 	if err != nil {
-		http.Error(w, "", http.StatusInternalServerError)
+		setupLog.Error(err, "Failed to generate caCert", "resourceName", resourceName)
+		http.Error(w, "Failed to generate caCert", http.StatusInternalServerError)
 		setupLog.Error(err, "failed to generate caKey")
 		return
 	}
 
 	caCert, err := certs.DecodeCertPEM(caCertRaw)
 	if err != nil {
-		http.Error(w, "", http.StatusInternalServerError)
+		setupLog.Error(err, "Failed to add API server", "resourceName", resourceName)
+		http.Error(w, "Failed to add API server", http.StatusInternalServerError)
 		setupLog.Error(err, "failed to generate caKey")
 		return
 	}
 	caKey, err := certs.DecodePrivateKeyPEM(caKeyRaw)
 	if err != nil {
-		http.Error(w, "", http.StatusInternalServerError)
+		setupLog.Error(err, "Failed to generate etcdKey", "resourceName", resourceName)
+		http.Error(w, "Failed to generate etcdKey", http.StatusInternalServerError)
 		setupLog.Error(err, "failed to generate caKey")
 		return
 	}
@@ -104,19 +128,21 @@ func register(w http.ResponseWriter, r *http.Request) {
 	apiServerPod1 := "kube-apiserver-1"
 	err = apiServerMux.AddAPIServer(resourceName, apiServerPod1, caCert, caKey.(*rsa.PrivateKey))
 	if err != nil {
-		http.Error(w, "", http.StatusInternalServerError)
+		setupLog.Error(err, "Failed to generate etcdCert", "resourceName", resourceName)
+		http.Error(w, "Failed to generate etcdCert", http.StatusInternalServerError)
 		setupLog.Error(err, "failed to add API server")
 		return
 	}
-	etcdKeyEncoded := r.URL.Query().Get("etcdKey")
+	etcdKeyEncoded := requestData.EtcdKey
 	etcdKeyRaw, err := base64.StdEncoding.DecodeString(etcdKeyEncoded)
 	if err != nil {
-		http.Error(w, "", http.StatusInternalServerError)
+		setupLog.Error(err, "Failed to add etcd member", "resourceName", resourceName)
+		http.Error(w, "Failed to add etcd member", http.StatusInternalServerError)
 		setupLog.Error(err, "failed to generate etcdKey")
 		return
 	}
 
-	etcdCertEncoded := r.URL.Query().Get("etcdCert")
+	etcdCertEncoded := requestData.EtcdCert
 	etcdCertRaw, err := base64.StdEncoding.DecodeString(etcdCertEncoded)
 	if err != nil {
 		http.Error(w, "", http.StatusInternalServerError)
@@ -154,14 +180,14 @@ func register(w http.ResponseWriter, r *http.Request) {
 	resp.Port = listener.Port()
 	data, err := json.Marshal(resp)
 	if err != nil {
-		setupLog.Error(err, "failed to Marshal the response")
+		setupLog.Error(err, "Failed to marshal the response", "resourceName", resourceName)
 		http.Error(w, "", http.StatusInternalServerError)
 		return
 	}
 
 	c, err := listener.GetClient()
 	if err != nil {
-		setupLog.Error(err, "failed to get listener client")
+		setupLog.Error(err, "Failed to get listener client", "resourceName", resourceName)
 		http.Error(w, "", http.StatusInternalServerError)
 		return
 	}
@@ -180,7 +206,7 @@ func register(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 	if err = c.Create(ctx, role); err != nil {
-		setupLog.Error(err, "failed to create cluster role")
+		setupLog.Error(err, "Failed to create cluster role", "resourceName", resourceName)
 		http.Error(w, "", http.StatusInternalServerError)
 		return
 	}
@@ -201,7 +227,7 @@ func register(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 	if err = c.Create(ctx, roleBinding); err != nil {
-		setupLog.Error(err, "failed to create cluster rolebinding")
+		setupLog.Error(err, "Failed to create cluster rolebinding", "resourceName", resourceName)
 		http.Error(w, "", http.StatusInternalServerError)
 		return
 	}
@@ -216,11 +242,12 @@ func register(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 	if err = c.Create(ctx, cm); err != nil {
-		setupLog.Error(err, "failed to create kubeadm configmap")
+		setupLog.Error(err, "Failed to create kubeadm configmap", "resourceName", resourceName)
 		http.Error(w, "", http.StatusInternalServerError)
 		return
 	}
-	if _, err = io.WriteString(w, string(data)); err != nil {
+	w.Header().Set("Content-Type", "application/json")
+	if _, err = w.Write(data); err != nil {
 		setupLog.Error(err, "failed to write the response data")
 		return
 	}
@@ -229,46 +256,122 @@ func register(w http.ResponseWriter, r *http.Request) {
 // updateNode receives nodeName and providerID, which are provided by CAPI after
 // node provisioning, from request, and update the Node object on the fake API server accordingly.
 func updateNode(w http.ResponseWriter, r *http.Request) {
-	resourceName := r.URL.Query().Get("resource")
-	listener := cloudMgr.GetResourceGroup(resourceName)
-	nodeName := r.URL.Query().Get("nodeName")
-	providerID := r.URL.Query().Get("providerID")
-	isControlPlane := r.URL.Query().Get("nodeType") == "CP"
+	var requestData struct {
+		ResourceName string `json:"resource"`
+		UUID         string `json:"uuid"`
+		NodeName     string `json:"nodeName"`
+		ProviderID   string `json:"providerID"`
+		NodeType     string `json:"nodeType"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		setupLog.Error(err, "Invalid JSON in request body")
+		return
+	}
+
+	listener := cloudMgr.GetResourceGroup(requestData.ResourceName)
+	timeOutput := metav1.Now()
+	setupLog.Info("Updating node", "resource: ", requestData.ResourceName, "nodeName: ", requestData.NodeName, "providerID:", requestData.ProviderID)
 
 	node := &corev1.Node{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: nodeName,
+			Name: requestData.NodeName,
+			Labels: map[string]string{
+				"metal3.io/uuid": requestData.UUID,
+			},
 		},
 		Spec: corev1.NodeSpec{
-			ProviderID: providerID,
+			ProviderID: requestData.ProviderID,
 		},
 		Status: corev1.NodeStatus{
 			Conditions: []corev1.NodeCondition{
 				{
+					LastHeartbeatTime:  timeOutput,
+					LastTransitionTime: timeOutput,
 					Type:               corev1.NodeReady,
 					Status:             corev1.ConditionTrue,
 				},
+				{
+					LastHeartbeatTime:  timeOutput,
+					LastTransitionTime: timeOutput,
+					Type:               corev1.NodeMemoryPressure,
+					Status:             corev1.ConditionFalse,
+					Message:            "kubelet has sufficient memory available",
+					Reason:             "KubeletHasSufficientMemory",
+				},
+				{
+					LastHeartbeatTime:  timeOutput,
+					LastTransitionTime: timeOutput,
+					Message:            "kubelet has no disk pressure",
+					Reason:             "KubeletHasNoDiskPressure",
+					Status:             corev1.ConditionFalse,
+					Type:               corev1.NodeDiskPressure,
+				},
+				{
+					LastHeartbeatTime:  timeOutput,
+					LastTransitionTime: timeOutput,
+					Message:            "kubelet has sufficient PID available",
+					Reason:             "KubeletHasSufficientPID",
+					Status:             corev1.ConditionFalse,
+					Type:               corev1.NodePIDPressure,
+				},
+				{
+					LastHeartbeatTime:  timeOutput,
+					LastTransitionTime: timeOutput,
+					Message:            "kubelet is posting ready status",
+					Reason:             "KubeletReady",
+					Status:             corev1.ConditionTrue,
+					Type:               corev1.NodeReady,
+				},
+			},
+			NodeInfo: corev1.NodeSystemInfo{
+				Architecture:    "amd64",
+				BootID:          "a4254236-e1e3-4462-97ed-4a25b8b29884",
+				OperatingSystem: "linux",
+				SystemUUID:      "1ce97e94-730c-42b7-98da-f7dcc0b58e93",
 			},
 		},
 	}
-	if isControlPlane {
-		node.Labels = map[string]string{}
+
+	if requestData.NodeType == "control-plane" {
 		node.Labels["node-role.kubernetes.io/control-plane"] = ""
 	}
 	c := listener.GetClient()
 	err := c.Create(ctx, node)
 	if err != nil {
-		http.Error(w, "", http.StatusInternalServerError)
+		if apierrors.IsAlreadyExists(err) {
+			setupLog.Info("Node already exists", "nodeName", requestData.NodeName)
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		logLine := fmt.Sprintf("Error adding node %s as type %s. Error: %s", requestData.NodeName, requestData.NodeType, err)
+		setupLog.Error(err, "Error adding node", "nodeName", requestData.NodeName, "nodeType", requestData.NodeType)
+		http.Error(w, logLine, http.StatusInternalServerError)
 		return
 	}
 }
 
 func main() {
-	ctrl.SetLogger(klog.Background())
+	log.SetLogger(zap.New(zap.UseDevMode(true)))
 	podIP := os.Getenv("POD_IP")
 	apiServerMux, _ = server.NewWorkloadClustersMux(cloudMgr, podIP)
-	http.HandleFunc("/register", register)
-	http.HandleFunc("/updateNode", updateNode)
+	setupLog.Info("Starting the FKAS server")
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		setupLog.Info("Received request to /register", "content", r.Body)
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		register(w, r)
+	})
+	http.HandleFunc("/updateNode", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		updateNode(w, r)
+	})
 	server := &http.Server{
 		Addr:         ":3333",
 		Handler:      nil,
@@ -277,6 +380,7 @@ func main() {
 		IdleTimeout:  15 * time.Second,
 	}
 	if err := server.ListenAndServe(); err != nil {
-		klog.Error(err.Error())
+		setupLog.Error(err, "Error starting server")
+		os.Exit(1)
 	}
 }
